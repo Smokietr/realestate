f741e004580edf1f9d0c481445ebe00e35cf23ce
Smokietr <ozgur@limeorlemon.com>
2021-09-14 07:27:12 +0300

    API Version

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,7 +26,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+    protected $apiNamespace ='App\Http\Controllers\API';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -38,14 +38,9 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::prefix('api')
-                ->middleware('api')
-                ->namespace($this->namespace)
+            Route::middleware(['api', 'api.version:v1'])
+                ->namespace($this->apiNamespace . '\V1')
                 ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/web.php'));
         });
     }
 
