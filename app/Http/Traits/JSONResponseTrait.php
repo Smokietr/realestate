f28fe98ffe598690212c4c6b9561466ff290b656
Smokietr <ozgur@limeorlemon.com>
2021-09-14 07:23:36 +0300

    Response Trait

---
+++ app/Http/Traits/JSONResponseTrait.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace App\Http\Traits;
+
+trait JSONResponseTrait
+{
+    /**
+     * @param $data
+     * @param int $statusCode
+     * @return \Illuminate\Http\JsonResponse
+    */
+
+    public function success($data = [], $statusCode = 200) {
+
+        $response = [
+            'success' => true,
+            'data' => $data
+        ];
+
+        if(is_null($response['data'])) {
+            unset($response['data']);
+        }
+
+        if(is_null($response['data'])) {
+            $statusCode = 204;
+        }
+
+        return response()->json($response)->setStatusCode($statusCode);
+    }
+
+    /**
+     * @param $errors
+     * @param int $statusCode
+     * @return \Illuminate\Http\JsonResponse|object
+    */
+    public function failed($errors = [], $statusCode = 422) {
+
+        $response = [
+            'success' => false
+        ];
+
+        if(!is_null($errors) && (is_array($errors) || is_object($errors))) {
+            foreach ($errors as $error) {
+                $response['error']['message'][] = $error;
+            }
+        }
+
+        return response()->json($response)->setStatusCode($statusCode);
+    }
+
+    public function unknownError() {
+        // Error 520: Web Server Is Returning an Unknown Error ( CloudFlare )
+        return $this->failed(['Unknown Error'], 520);
+        // $response['error']['message'] = 'Unknown Error';
+    }
+}
