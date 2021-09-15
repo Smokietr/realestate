30fbfd40aafb2baa35e732f4dd73f48ada0c094a
Smokietr <ozgur@limeorlemon.com>
2021-09-15 23:49:15 +0300

    Customer Paginate

--- app/Http/Controllers/API/V1/Customers/HomeController.php
+++ app/Http/Controllers/API/V1/Customers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API\V1\Customers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\API\V1\Customers\IndexRequest;
 use App\Http\Requests\API\V1\Customers\StoreRequest;
 use App\Http\Requests\API\V1\Customers\UpdateRequest;
 use App\Http\Resources\API\V1\Customers\IndexResource;
@@ -16,7 +17,7 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function index()
+    public function index(IndexRequest $request)
     {
         return $this->success(IndexResource::collection(Customers::paginate(10)));
     }
